85abc6f1cc3a2daede799edf536531446bdf2c5b
Kolan Yamini <Kolan Yamini@192.168.0.8>
2020-04-22 17:36:47 +0530

    commit

---
+++ limits-services/src/main/java/com/microservices/limits/services/LimitsConfigurationController.java
@@ -0,0 +1,16 @@
+package com.microservices.limits.services;
+
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatpoint.microservices.limitservice.bean.LimitConfiguration;  
+ 
+@RestController  
+public class LimitsConfigurationController   
+{  
+@GetMapping("/limits")  
+public LimitConfiguration retriveLimitsFromConfigurations()  
+{  
+return new LimitConfiguration(1000, 1);  
+}  
+}  
---
+++ limits-services/src/main/java/com/microservices/limits/services/LimitsServicesApplication.java
@@ -0,0 +1,14 @@
+package com.microservices.limits.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServicesApplication.class, args);
+		System.out.println("hii");
+	}
+
+}
